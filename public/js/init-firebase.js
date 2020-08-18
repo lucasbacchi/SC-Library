1f6ba51e7825084d4574610f3f8d42b9e0b26057
Lucas Bacchi <lucasbacchi22@gmail.com>
2020-08-17 20:46:56 -0400

    add firebase config

--- public/js/init-firebase.js
+++ public/js/init-firebase.js
@@ -1,7 +1,14 @@
-// TODO: Replace the following with your app's Firebase project configuration
+
 var firebaseConfig = {
-    // ...
-  };
-  
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+    apiKey: "AIzaSyAsQp9MoNHpx_082eyH4gi-K-M1v1YGzKU",
+    authDomain: "south-church-library.firebaseapp.com",
+    databaseURL: "https://south-church-library.firebaseio.com",
+    projectId: "south-church-library",
+    storageBucket: "south-church-library.appspot.com",
+    messagingSenderId: "695036619849",
+    appId: "1:695036619849:web:5e99fa7a4f64cec2daa398",
+    measurementId: "G-MRXNED32DZ"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
