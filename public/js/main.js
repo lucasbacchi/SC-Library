2cc41e546b35350345c40a9a15de12f769c3c23b
FunMan22 <43394783+FunMan22@users.noreply.github.com>
2020-08-17 22:24:43 -0400

    fix require.js??

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,9 @@
-
+requirejs(["helper/util"], function(util) {
+    //This function is called when scripts/helper/util.js is loaded.
+    //If util.js calls define(), then this function is not fired until
+    //util's dependencies have loaded, and the util argument will hold
+    //the module value for "helper/util".
+    console.log("A message triggered by the loading of util.js.");
+});
 
 console.log("main.js Loaded!")
