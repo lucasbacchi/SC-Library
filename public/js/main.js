ebc84c8830bb27b13cedfe0bb8010045679da4a2
FunMan22 <43394783+FunMan22@users.noreply.github.com>
2020-08-19 13:16:58 -0400

    update nav for mobile compatibility

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,28 @@
 
+$(document).ready(function(){
+    if ($(document).width() <= 500) {
+        $('#hamburger-button').click(function() {
+            $('nav').width('60%');
+            $('nav > li > a').show();
+            $('#close-button').css('display', 'block');
 
+        });
+
+        $('#close-button').click(function() {
+            $('nav').width('0');
+            $('#close-button').hide();
+            $('nav > li > a').hide();
+        })
+    }
+
+
+    $(docuemnt).resize(function() {
+        if ($(docuemnt).width() > 500) {
+            $('nav').width('60%');
+        }
+    });
+
+});
 
 
 console.log("main.js Loaded!");
