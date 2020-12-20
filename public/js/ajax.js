c6951517c88acd6aec4786a5240c0b2f62075d26
Lucas Bacchi <lucasbacchi22@gmail.com>
2020-12-19 19:07:00 -0500

    move to clean URLs

--- public/js/ajax.js
+++ public/js/ajax.js
@@ -1,35 +1,40 @@
-var xhttp = new XMLHttpRequest();
-
 var url = window.location.href;
 var path = window.location.pathname;
 var query = window.location.search;
 var hash = window.location.hash;
+
+console.log(window.history.state);
+var stateObject = {};
+history.replaceState(stateObject, '', path.substr(0, path.indexOf(".")) + query + hash);
+console.log(window.history.state);
 
 console.log(path);
 console.log(hash);
 
 // Search Content folder
 var directory = [
-    "/admin/addEntry.html",
-    "/admin/editEntry.html",
-    "/admin/main.html",
-    "/admin/report.html",
-    "/404.html",
-    "/about.html",
-    "/account.html",
-    "/advancedSearch.html",
-    "/autogenindex.html",
-    "/help.html",
-    "/login.html",
-    "/main.html",
-    "/search.html",
-    "/signup.html",
-    "/sitemap.html"
+    "/admin/addEntry",
+    "/admin/editEntry",
+    "/admin/main",
+    "/admin/report",
+    "/404",
+    "/about",
+    "/account",
+    "/advancedSearch",
+    "/autogenindex",
+    "/help",
+    "/login",
+    "/main",
+    "/search",
+    "/signup",
+    "/sitemap"
 ];
+
+var xhttp = new XMLHttpRequest();
 
 // Get the content of the page
 if (directory.includes(path)){
-    xhttp.open("GET", "/content" + path + query + hash, true);
+    xhttp.open("GET", "/content" + path + ".html" + query + hash, true);
 } else if (path == "/" || path == "/index.html") {
     xhttp.open("GET", "/content/main.html" + query + hash, true);
 } else {
@@ -52,6 +57,11 @@ $(document).ready(function() {
     }
 
 });
+
+window.onbeforeunload = function() {
+    history.replaceState(stateObject, '', path + query + hash);
+    console.log("The path on unload is " + path);
+};
 
 
 console.log("ajax.js has Loaded!");
