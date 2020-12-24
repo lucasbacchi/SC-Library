a5205e85f1a4d1e6427b3181dc5357f0bddad352
FunMan22 <43394783+FunMan22@users.noreply.github.com>
2020-12-24 15:16:59 -0500

    update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,4 @@
+// Manage Menu Button event listener
 $('#hamburger-button').click(function() {
     $('nav').width('60%');
     $('nav > li > a').show();
@@ -7,6 +8,7 @@ $('#hamburger-button').click(function() {
 
 });
 
+// Manage Menu Close Button event listener
 $('#close-button').click(function() {
     $('nav').width('0');
     $('#close-button').delay(400).hide(0);
@@ -15,7 +17,7 @@ $('#close-button').click(function() {
     $('nav > li > a').delay(400).hide(0);
 });
 
-
+// Manage Nav Links when screen gets small
 $(window).resize(function () {
     /*console.log($(window).width());*/
     if ($(window).width() > 500) {
@@ -33,61 +35,72 @@ $(window).resize(function () {
     }
 });
 
-function setResize() {
-    /*console.log($(window).width());*/
-    if ($(window).width() > 500) {
-        $('nav').width('fit-content');
-        $('nav > li > a').show();
-        $('nav > li > a').css('opacity', '1');
-        $('#close-button').hide();
+/* WHAT WAS THIS CODE FOR???
+if ($(window).width() > 500) {
+    $('nav').width('fit-content');
+    $('nav > li > a').show();
+    $('nav > li > a').css('opacity', '1');
+    $('#close-button').hide();
 
-    }
-    if ($(window).width() <= 500 && $('#close-button').css('display') == 'none') {
-        $('nav').width('0');
-        $('nav > li > a').hide();
-        $('nav > li > a').css('opacity', '0');
-
-    }
 }
+if ($(window).width() <= 500 && $('#close-button').css('display') == 'none') {
+    $('nav').width('0');
+    $('nav > li > a').hide();
+    $('nav > li > a').css('opacity', '0');
 
-var largeAccountOpen = false;
-$('#small-account-container').click(function() {
-    if($('#large-account-container').css('display') == 'none') {
-        setTimeout(function() {largeAccountOpen = true;}, 20);
-        $('#large-account-container').show(0).delay(10);
-        $('#large-account-container').css('right', '0%');
-    } else {
-        closeLargeAccount();
-    }
-
-});
-
-function closeLargeAccount() {
-    largeAccountOpen = false;
-    $('#large-account-container').delay(1000).hide(0);
-    $('#large-account-container').css('right', '-500%');
-}
+}*/
 
 
-$(window).click(function(event) {
-    if (!($.contains($('#large-account-container')[0], /*$(*/event.target/*).parents()*/) || event.target == $('#large-account-container')[0])) {
-        if (largeAccountOpen) {
+// Manage Account Panel and animation
+{
+    let largeAccountOpen = false;
+    $('#small-account-container').click(function() {
+        if($('#large-account-container').css('display') == 'none') {
+            setTimeout(function() {largeAccountOpen = true;}, 20);
+            $('#large-account-container').show(0).delay(10);
+            $('#large-account-container').css('right', '0%');
+        } else {
             closeLargeAccount();
         }
+
+    });
+
+
+    function closeLargeAccount() {
+        largeAccountOpen = false;
+        $('#large-account-container').delay(400).hide(0);
+        $('#large-account-container').css('right', '-500%');
     }
 
-});
+
+    $(window).click(function(event) {
+        if (!($.contains($('#large-account-container')[0], event.target) || event.target == $('#large-account-container')[0])) {
+            if (largeAccountOpen) {
+                closeLargeAccount();
+            }
+        }
+
+    });
+}
 
 
 
 
 
 /* AUTHENTICATION START */
+
+initApp()
+.then(function() {
+    console.log("Autentication has started succesfully.")
+}, function(error) {
+    console.log(error);
+});
+
 function signOut() {
     if (firebase.auth().currentUser) {
         firebase.auth().signOut();
         /* could change 'replace' to 'href' if we wanted to keep the page in the history */
-        window.location.replace('./')
+        window.location.replace('./');
     } else {
         alert("No user is currently signed in.");
     }
@@ -99,7 +112,7 @@ function signOut() {
  *    out, and that is where we update the UI.
  */
 function initApp() {
-    // Listening for auth state changes.]
+    // Listening for auth state changes.
     console.log("Firebase Authentication is Initalizing.");
     return new Promise(function(resolve, reject) {
         try {
@@ -108,39 +121,11 @@ function initApp() {
                 if (user) {
                     // User is signed in.
                     console.log('User is now Signed In.');
-                    var displayName = user.displayName;
-                    $('#account-name').text(displayName);
-                    var email = user.email;
-                    $('#account-email').text(email);
-                    var emailVerified = user.emailVerified;
-                    var photoURL = user.photoURL;
-                    if (photoURL != null) {
-                        $('#small-account-image, #small-account-image').attr('src', photoURL);
-                    }
-                    var isAnonymous = user.isAnonymous;
-                    var uid = user.uid;
-                    var providerData = user.providerData;
-                    if (!emailVerified) {
-                        // User's email is not verified
-                    }
-                    $('#nav-login-signup').hide();
-                    $('#small-account-container').show();
-                    $('#large-account-image').show();
-                    $('#large-account-image').show();
-                    $('#account-email').show();
-                    $('#account-settings').show();
-                    $('#log-out').html('<a>Log Out</a>').css('width', '50%').attr('onclick', 'javascript:signOut();');
                 } else {
                     // User is signed out.
                     console.log('User is now Signed Out.');
-                    $('#nav-login-signup').show();
-                    $('#small-account-container').hide();
-                    $('#large-account-image').hide();
-                    $('#account-email').hide();
-                    $('#account-name').html('No user signed in');
-                    $('#account-settings').hide();
-                    $('#log-out').html('<a href="login.html">Log In</a>').css('width', '100%').attr('onclick', '');
                 }
+                updateUserAccountInfo();
                 resolve();
             })
         } catch(err) {
@@ -149,28 +134,50 @@ function initApp() {
     });
 }
 
-initApp()
-.then(function() {
-    var currentUser = firebase.auth().currentUser;
-    if (currentUser != null) {
+
+
+function updateUserAccountInfo() {
+    var user = firebase.auth().currentUser;
+    if (user) {
         // User is signed in.
-        console.log("Page Loaded with a User Signed In.");
-        if (!currentUser.emailVerified) {
+
+        var displayName = user.displayName;
+        $('#account-name').text(displayName);
+        var email = user.email;
+        $('#account-email').text(email);
+        var emailVerified = user.emailVerified;
+        var photoURL = user.photoURL;
+        if (photoURL != null) {
+            $('#small-account-image, #small-account-image').attr('src', photoURL);
+        }
+        var isAnonymous = user.isAnonymous;
+        var uid = user.uid; // Not to be used for authentication or identification (User.getToken();)
+        var providerData = user.providerData; // Should not have any with email signup
+        if (!emailVerified) {
             // User's email is not verified
         }
+
+        // Change Account Container Appearence
+        $('#nav-login-signup').hide();
+        $('#small-account-container').show();
+        $('#large-account-image').show();
+        $('#large-account-image').show();
+        $('#account-email').show();
+        $('#account-settings').show();
+        $('#log-out').html('<a>Log Out</a>').css('width', '50%').attr('onclick', 'javascript:signOut();');
     } else {
         // User is signed out.
-        console.log("Page Loaded without a user signed in.")
+
+        // Change Account Container Appearence
+        $('#nav-login-signup').show();
+        $('#small-account-container').hide();
+        $('#large-account-image').hide();
+        $('#account-email').hide();
+        $('#account-name').html('No user signed in');
+        $('#account-settings').hide();
+        $('#log-out').html('<a href="login.html">Log In</a>').css('width', '100%').attr('onclick', '');
     }
-}, function(error) {
-    console.log(error);
-});
-
-
-
-
-
-setResize();
+}
 
 
 console.log("main.js Loaded!");
