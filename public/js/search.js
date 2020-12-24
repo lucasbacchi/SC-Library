c765914af03d16b03c7006ca19c5ee56dc884b17
FunMan22 <43394783+FunMan22@users.noreply.github.com>
2020-12-24 16:31:25 -0500

    rework back end source loading

--- public/js/search.js
+++ public/js/search.js
@@ -1,6 +1,6 @@
-
+// Make content Responsive
+// Doesn't have to be setup because the window element doesn't change.
 $(window).resize(function () {
-    /*console.log($(window).width());*/
     if ($(window).width() > 500) {
         $('#sort-container').width('fit-content');
         $('.sort-section').show();
@@ -18,33 +18,32 @@ $(window).resize(function () {
     }
 });
 
-function setResize2() {
-    /*console.log($(window).width());*/
-    if ($(window).width() > 500) {
-        $('.sort-section').show();
+// Set Initial window layout.
+if ($(window).width() > 500) {
+    $('.sort-section').show();
 
-    }
-    if ($(window).width() <= 500) {
-        $('.sort-section').hide();
+}
+if ($(window).width() <= 500) {
+    $('.sort-section').hide();
 
-    }
 }
 
-setResize2();
-
-$('#sort-main-title').click(function() {
-    if ($('.sort-section').css('display') == 'none') {
-        $('.sort-section').show(0).delay(10);
-        $('#sort-sidebar').css('max-height', '500px');
-        $('#sort-sidebar').css('overflow-y', 'scroll');
-        $('.sort-section').css('opacity', '1');
-    } else {
-        $('.sort-section').delay(1000).hide(0);
-        $('#sort-sidebar').css('overflow-y', 'hidden');
-        $('#sort-sidebar').css('max-height', '58px');
-        $('.sort-section').css('opacity', '0');
-    }
-});
+function setupSearch() {
+    // Create Sort Dropdown Event Listener
+    $('#sort-main-title').click(function() {
+        if ($('.sort-section').css('display') == 'none') {
+            $('.sort-section').show(0).delay(10);
+            $('#sort-sidebar').css('max-height', '500px');
+            $('#sort-sidebar').css('overflow-y', 'scroll');
+            $('.sort-section').css('opacity', '1');
+        } else {
+            $('.sort-section').delay(1000).hide(0);
+            $('#sort-sidebar').css('overflow-y', 'hidden');
+            $('#sort-sidebar').css('max-height', '58px');
+            $('.sort-section').css('opacity', '0');
+        }
+    });
+}
 
 
 console.log("search.js Loaded!");
