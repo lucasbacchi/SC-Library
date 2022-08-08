bd2c1f2d5587410c9c8795241240150d21adc360
justinbacchi73 <30395921+justinbacchi73@users.noreply.github.com>
2022-08-08 00:54:15 -0400

    filters at least sort of works now

--- public/js/search.js
+++ public/js/search.js
@@ -65,6 +65,10 @@ export function setupSearch(searchResultsArray, pageQuery) {
         searchPageSearch();
     });
 
+    $("#apply-filters-button").on("click", () => {
+        applySearchFilters();
+    });
+
     var queryFromURL = findURLValue(pageQuery, "query", true);
 
     $("#search-page-input").val(queryFromURL);
@@ -73,18 +77,14 @@ export function setupSearch(searchResultsArray, pageQuery) {
         // If you are entering the page without a search completed
         if (queryFromURL == "") {
             browse();
-            return;
+        } else {
+            search(queryFromURL).then((resultsArray) => {
+                createSearchResultsPage(resultsArray);
+            });
         }
-        search(queryFromURL).then((resultsArray) => {
-            createSearchResultsPage(resultsArray);
-        });
     } else {
         createSearchResultsPage(searchResultsArray);
     }
-
-    $("#apply-search-filters").on("click", () => {
-        applySearchFilters();
-    });
 }
 
 function searchPageSearch() {
@@ -206,18 +206,18 @@ function createFilterList(searchResultsArray) {
 
     // Authors
     for (let i = 0; i < searchResultsArray.length; i++) {
-        if (!searchResultsAuthorsArray.includes(searchResultsArray[i].authors[0]) && searchResultsArray[i].authors[0]) {
-            searchResultsAuthorsArray.push(searchResultsArray[i].authors[0]);
-        }
-        if (!searchResultsAuthorsArray.includes(searchResultsArray[i].authors[1]) && searchResultsArray[i].authors[1]) {
-            searchResultsAuthorsArray.push(searchResultsArray[i].authors[1]);
+        for (let j = 0; j < 2; j++) {
+            let authorString = searchResultsArray[i].authors[j]?.last + ", " + searchResultsArray[i].authors[j]?.first;
+            if (!searchResultsAuthorsArray.includes(authorString) && authorString != "undefined, undefined") {
+                searchResultsAuthorsArray.push(authorString);
+            }
         }
     }
     for (let i = 0; i < searchResultsAuthorsArray.length; i++) {
-        let authorArray = searchResultsAuthorsArray[i];
+        let authorString = searchResultsAuthorsArray[i];
         const li = document.createElement("li");
         li.classList.add("sort-item");
-        li.innerHTML = "<input type=\"checkbox\">" + authorArray.last + ", " + authorArray.first;
+        li.innerHTML = "<input type=\"checkbox\"><span>" + authorString + "</span";
         if (i < 6) {
             $("#sort-author-list")[0].appendChild(li);
         } else if (i == 6) {
@@ -228,10 +228,10 @@ function createFilterList(searchResultsArray) {
             $("#author-show-more").on("click", () => {
                 $("#author-show-more").css("display", "none");
                 for (let j = 6; j < searchResultsAuthorsArray.length; j++) {
-                    let authorArray = searchResultsAuthorsArray[j];
+                    let authorString = searchResultsAuthorsArray[j];
                     const li = document.createElement("li");
                     li.classList.add("sort-item");
-                    li.innerHTML = "<input type=\"checkbox\">" + authorArray.last + ", " + authorArray.first;
+                    li.innerHTML = "<input type=\"checkbox\"><span>" + authorString + "</span>";
                     $("#sort-author-list")[0].appendChild(li);
                 }
             });
@@ -250,7 +250,7 @@ function createFilterList(searchResultsArray) {
         let subject = searchResultsSubjectsArray[i];
         const li = document.createElement("li");
         li.classList.add("sort-item");
-        li.innerHTML = "<input type=\"checkbox\">" + subject;
+        li.innerHTML = "<input type=\"checkbox\"><span>" + subject + "</span>";
         if (i < 6) {
             $("#sort-subject-list")[0].appendChild(li);
         } else if (i == 6) {
@@ -264,7 +264,7 @@ function createFilterList(searchResultsArray) {
                     let subject = searchResultsSubjectsArray[j];
                     const li = document.createElement("li");
                     li.classList.add("sort-item");
-                    li.innerHTML = "<input type=\"checkbox\">" + subject;
+                    li.innerHTML = "<input type=\"checkbox\"><span>" + subject + "</span>";
                     $("#sort-subject-list")[0].appendChild(li);
                 }
             });
@@ -591,36 +591,67 @@ function applySearchFilters() {
         filters.push($(".sort-section")[i].children[0].innerHTML);
         items.push([]);
         for (let j = 0; j < $(".sort-section")[i].children[1].children.length; j++) {
-            if ($(".sort-section")[i].children[1].children[j].tagName == "LI") {
-                items[items.length - 1].push($(".sort-section")[i].children[1].children[j].children[0].checked);
+            if ($(".sort-section")[i].children[1].children[j].tagName == "LI" &&
+                $(".sort-section")[i].children[1].children[j].children[0].checked) {
+                items[items.length - 1].push($(".sort-section")[i].children[1].children[j].children[1].innerHTML);
             }
         }
-        if (items[items.length - 1].every((val, q, arr) => val === arr[0])) {
+        if (items[items.length - 1].length == 0) {
             filters.pop();
             items.pop();
         }
     }
-    for (let i = 0, passesFilters = true; i < searchCache.length && passesFilters; i++) {
-        for (let j = 0; j < filters.length; j++) {
-            var passesFilter = false;
+    if (!searchCache) {
+        search("").then(() => {
+            searchWithFilters(filters, items, results);
+        });
+    } else {
+        searchWithFilters(filters, items, results);
+    }
+}
+
+function searchWithFilters(filters, items, results) {
+    console.log("searchCache:");
+    console.log(searchCache);
+    for (let i = 0; i < searchCache.length; i++) {
+        for (let j = 0, passesAllFilters = true; j < filters.length && passesAllFilters; j++) {
+            var passesFilter = true;
             for (let k = 0; k < items[j].length; k++) {
-                if (filters[j] == "Audience") {
-                    if (items[j][k] && searchCache[i].audience[k]) {
-                        passesFilter = true;
+                if (filters[j] == "Author") {
+                    if (items[j][k] != searchCache[i].authors[0].last + ", " + searchCache[i].authors[0].first &&
+                        searchCache[i].authors[1] &&
+                        items[j][k] != searchCache[i].authors[1].last + ", " + searchCache[i].authors[1].first) {
+                        passesFilter = false;
                     }
                 } else if (filters[j] == "Medium") {
-                    if (searchCache[i].medium == items[j][k]) {
-                        // Justin, finish this
+                    if (items[j][k].toUpperCase() != searchCache[i].medium.toUpperCase()) {
+                        passesFilter = false;
+                    }
+                } else if (filters[j] == "Audience") {
+                    if (items[j][k] == "Children" && !searchCache[i].audience[0] ||
+                        items[j][k] == "Youth" && !searchCache[i].audience[1] ||
+                        items[j][k] == "Adult" && !searchCache[i].audience[2]) {
+                        passesFilter = false;
+                    }
+                } else if (filters[j] == "Subject") {
+                    if (!searchCache[i].subjects.includes(items[j][k])) {
+                        passesFilter = false;
+                    }
+                } else if (filters[j] == "Type") {
+                    if (items[j][k] == "Non-fiction" && searchCache.ddc == "FIC" ||
+                        items[j][k] == "Fiction" && searchCache.ddc != "FIC") {
+                        passesFilter = false;
                     }
                 }
+                if (!passesFilter)
+                    passesAllFilters = false;
             }
-            if (!passesFilter)
-                passesFilters = false;
-        }
-        if (passesFilters) {
-            results.push(searchCache[i]);
+            if (passesAllFilters) {
+                results.push(searchCache[i]);
+            }
         }
     }
+
     createSearchResultsPage(results);
 }
 
